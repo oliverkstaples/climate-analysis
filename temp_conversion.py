b2635ba030f1c466f92a79bac3692d39f3186f68
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-10-16 12:10:24 +0100

    Changed more variable names

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,11 +1,11 @@
 # Todo: Code is a bit unclear
 
-def fahrenheit_to_celcius(x):
-    celcius = (x - 32) * (5 / 9)
+def fahrenheit_to_celcius(fahr):
+    celcius = (fahr - 32) * (5 / 9)
     return celcius
 
-def fahrenheit_to_kelvin(x):
-    celcius = fahrenheit_to_celcius(x)
+def fahrenheit_to_kelvin(fahr):
+    celcius = fahrenheit_to_celcius(fahr)
     kelvin = celcius + 273.15
     return kelvin
 
