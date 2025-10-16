79df775b374439cec7645b4b6c302f080bf0e203
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-10-16 12:19:20 +0100

    Update temp_conversion.py with new logic

--- temp_conversion.py
+++ temp_conversion.py
@@ -9,5 +9,5 @@ def fahrenheit_to_kelvin(fahr):
     kelvin = celcius + 273.15
     return kelvin
 
-print(fahrenheit_to_celcius(212))   # Expect 100.0
-print(fahrenheit_to_kelvin(32))     # Expect 273.15
+# print(fahrenheit_to_celcius(212))   # Expect 100.0
+# print(fahrenheit_to_kelvin(32))     # Expect 273.15
