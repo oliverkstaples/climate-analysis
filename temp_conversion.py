24e7ec24b2e413a0540be24f1a7db6c6896fbcc1
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-10-16 11:22:47 +0100

    Changed the function names

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,10 @@
 # Todo: Code is a bit unclear
 
-def Fc(x):
+def F_to_C(x):
     Y = (x - 32) * (5 / 9)
     return Y
 
 def FK(x):
-    y = Fc(x)
+    y = F_to_C(x)
     z = y + 273.15
     return z
