d5395d95b3e28c6d23d6fa021f80695f7cac7664
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-10-16 14:28:56 +0100

    Changed name to fahr_to_celsius and fahr_to_kelvin

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,11 +1,11 @@
 # Todo: Code is a bit unclear
 
-def fahrenheit_to_celcius(fahr):
+def fahr_to_celsius(fahr):
     celcius = (fahr - 32) * (5 / 9)
     return celcius
 
-def fahrenheit_to_kelvin(fahr):
-    celcius = fahrenheit_to_celcius(fahr)
+def fahr_to_kelvin(fahr):
+    celcius = fahr_to_celsius(fahr)
     kelvin = celcius + 273.15
     return kelvin
 
